06257df90293099f1ada13bdd3816a68bbc72da1
sabesh-s <sabeshsps@gmail.com>
2024-03-19 19:12:34 +0530

    first commit

---
+++ Error Makes Clever Academy/demo.js
@@ -0,0 +1,4 @@
+for(count=1;count<=20;count=count+1)
+{
+    console.log(count+"x2="+count*2)
+}
---
+++ Logic First JS/String/script.js
@@ -0,0 +1,8 @@
+let usersname="Sabesh";
+console.log(usersname.charAt(Math.floor(Math.random()*6)))
+
+let units=5
+let amount=95
+let msg = `"You have ${units} items in your cart.
+Your bill amount is $${amount}"`
+console.log(msg)
